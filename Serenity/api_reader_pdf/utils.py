544952814e2a6469d2a78d865b91a91da35504bd
Angel Ceron Ramos <angelceronramos2201@gmail.com>
2023-06-07 19:20:09 -0600

    Update utils.py

--- Serenity/api_reader_pdf/utils.py
+++ Serenity/api_reader_pdf/utils.py
@@ -5,7 +5,7 @@ from PyPDF2 import PdfReader
 from .models import *
 
 
-openai.api_key = "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"
+openai.api_key = ""
 modelo = "text-davinci-003"
 tokenizer = tiktoken.get_encoding("cl100k_base")
 max_tokens = 1000
