2c6b4f9c1729c1eabcc091c2cdb83d26afa255d8
Angel Ceron Ramos <angelceronramos2201@gmail.com>
2023-06-13 10:39:42 -0600

    s

--- Serenity/chatbot/urls.py
+++ Serenity/chatbot/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.chatbot)
+    path('', views.chatbot, name='home'),
+    path('login/', views.login_view, name='login')
 ]
--- Serenity/chatbot/views.py
+++ Serenity/chatbot/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def chatbot(request):
     return render(request, 'chatbot/index.html')
+
+def login_view(request):
+    return render(request, 'chatbot/sign-in.html')
