a3d0ff8210853a258055a44f58cdcbc104084507
Angel Ceron Ramos <angelceronramos2201@gmail.com>
2023-06-27 10:52:37 -0600

    v

--- Serenity/api_reader_pdf/utils.py
+++ Serenity/api_reader_pdf/utils.py
@@ -9,7 +9,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 
 
-openai.api_key = "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"
+openai.api_key = ""
 modelo = "text-davinci-003"
 tokenizer = tiktoken.get_encoding("cl100k_base")
 max_tokens = 1000
@@ -204,7 +204,7 @@ def answer_question(identificador, model="text-davinci-003", question='', max_le
         # Create a completions using the question and context
         response = openai.Completion.create(
             prompt = f"Soy un chatbot que puede leer y responder preguntas basadas en el contexto de un PDF. Si no puedo responder tu pregunta, diré 'No lo sé'. Si deseas saber mi opinión, pregunta '¿Qué piensas tú?', '¿Cuál es tu punto de vista?' o algo similar. También puedes preguntarme cómo estoy o cualquier otra pregunta abierta. ¡Estoy aquí para ayudarte! \n\nContexto: {context}\n\nPregunta: {question}\n\nRespuesta: ",
-            temperature=0.4,
+            temperature=0.1,
             max_tokens=max_tokens,
             top_p=1,
             frequency_penalty=0,
